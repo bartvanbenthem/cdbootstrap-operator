0fdb4180dbdec4fe1b62daba1a6a82ed5b02ed25
bartvanbenthem <bartvanbenthem@hotmail.com>
2024-01-07 17:55:04 +0100

    upd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Ok;
+use std::sync::Arc;
+use kube::{client::Client, runtime::controller::Action, runtime::Controller, Api};
+
+use crate::crd::CDBootstrap;
+
+pub mod crd;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // First, a Kubernetes client must be obtained using the `kube` crate
+    // The client will later be moved to the custom controller
+    let kubernetes_client: Client = Client::try_default()
+        .await
+        .expect("Expected a valid KUBECONFIG environment variable.");
+
+    // Preparation of resources used by the `kube_runtime::Controller`
+    let crd_api: Api<CDBootstrap> = Api::all(kubernetes_client.clone());
+    let context: Arc<ContextData> = Arc::new(ContextData::new(kubernetes_client.clone()));
+
+    Ok(())
+}
+
+/// Context injected with each `reconcile` and `on_error` method invocation.
+struct ContextData {
+    /// Kubernetes client to make Kubernetes API requests with. Required for K8S resource management.
+    client: Client,
+}
+
+impl ContextData {
+    /// Constructs a new instance of ContextData.
+    ///
+    /// # Arguments:
+    /// - `client`: A Kubernetes client to make Kubernetes REST API requests with. Resources
+    /// will be created and deleted with this client.
+    pub fn new(client: Client) -> Self {
+        ContextData { client }
+    }
 }
