7024b58426a1860c771ac1e55a9e3a96106e4be5
bartvanbenthem <bartvanbenthem@hotmail.com>
2024-01-11 09:24:06 +0100

    upd sh

---
+++ remove.sh
@@ -0,0 +1,2 @@
+kubectl delete -f cdbootstrap-example.yaml
+kubectl delete -f cdbootstraps.cnad.nl.yaml
--- run.sh
+++ run.sh
@@ -5,5 +5,3 @@ kubectl apply -f cdbootstraps.cnad.nl.yaml
 cargo run
 
 # kubectl apply -f cdbootstrap-example.yaml
-# kubectl delete -f cdbootstrap-example.yaml
-# kubectl delete -f cdbootstraps.cnad.nl.yaml
