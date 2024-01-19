090300e1540b3ff76b4d64ceafddf796d4fc909d
bartvanbenthem <bartvanbenthem@hotmail.com>
2024-01-19 14:09:24 +0100

    upd

--- remove.sh
+++ remove.sh
@@ -1,2 +1,2 @@
 kubectl delete -f config/samples/cdbootstrap-example.yaml
-kubectl delete -f config/crd/cdbootstraps.wasm.cloud.yaml
+kubectl delete -f config/crd/cdbootstraps.operators.cloud.yaml
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 KUBECONFIG=~/.kube/k3s.yaml
 
-kubectl create -f config/crd/cdbootstraps.wasm.cloud.yaml
+kubectl create -f config/crd/cdbootstraps.operators.cloud.yaml
 
 cargo fmt
 cargo run
