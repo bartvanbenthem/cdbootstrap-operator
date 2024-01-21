e5f7016c9e2d31502589e150c125b6a6cc7f93f5
bartvanbenthem <bartvanbenthem@hotmail.com>
2024-01-21 11:03:27 +0100

    update

--- run.sh
+++ run.sh
@@ -8,4 +8,7 @@ cargo run
 # kubectl apply -f config/samples/cdbootstrap-example.yaml
 # kubectl apply -f config/samples/update.yaml
 
-# kubectl patch secret test-bootstrap -p '{"data":{"AZP_TOKEN":"cGFzc3dvcmQ="}}'
+# source ../00-ENV/env.sh
+# export EPAT=$(echo $PAT | base64)
+# kubectl patch secret test-bootstrap -p '{"data":{"AZP_TOKEN": "'"$EPAT"'"}}'
+# kubectl scale deploy test-bootstrap --replicas=0 && kubectl scale deploy test-bootstrap --replicas=2
