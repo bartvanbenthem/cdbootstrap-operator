5d5c58982a1cbfe459893c00b73702002ea28152
bartvanbenthem <bartvanbenthem@hotmail.com>
2024-02-14 19:40:11 +0100

    update logs

--- src/vault.rs
+++ src/vault.rs
@@ -73,22 +73,24 @@ pub async fn run(client: Client, name: &str, namespace: &str) {
     let sps = secret_value_is_set(client.clone(), &name, &namespace, "SPN_SECRET").await;
     let azp = secret_value_is_set(client.clone(), &name, &namespace, "AZP_TOKEN").await;
     if azp == false && sps == false {
-        info!("Make sure to inject the AZP_TOKEN, or set the SPN_SECRET to collect a Token from the Vault");
+        info!("Make sure to inject the AZP_TOKEN in Namespace {}, or set the SPN_SECRET to collect a Token from the Vault",
+        namespace);
     }
 
     if sps == true && azp == false {
+        info!("SPN_SECRET value in Namespace {} Has been set", namespace);
         info!("Testing authentication to the Vault");
         AzureVault::test_connection().await;
     }
 
     if azp == true {
-        info!("AZP_TOKEN Has been set, check the Agent logs for polling state");
+        info!("AZP_TOKEN value in Namespace {} has been SET", namespace);
+        info!("Check the Pod logs to see if the Agent is polling")
     }
 }
 
 async fn secret_value_is_set(client: Client, name: &str, namespace: &str, key: &str) -> bool {
     let mut is_set = false;
-
     let api: Api<Secret> = Api::namespaced(client.clone(), namespace);
     if let Ok(secret) = api.get(name).await {
         let data = secret.data.unwrap();
@@ -96,21 +98,14 @@ async fn secret_value_is_set(client: Client, name: &str, namespace: &str, key: &
             let token_decoded = from_utf8(&value.0).unwrap_or("unable to decode Secret value");
             //println!("Found secret data: {:?}",token_decoded.replace("\n", ""));
             if token_decoded == "" {
-                info!(
-                    "{} Secret {} in namespace {} has NOT been set or collected",
-                    key, name, namespace
-                );
+                is_set = false;
             } else {
-                info!(
-                    "{} Secret {} in namespace {} has a value",
-                    key, name, namespace
-                );
                 is_set = true;
             }
         }
         is_set
     } else {
-        error!("Secret {} in namespace {} not found", name, namespace);
+        error!("Secret {} in namespace {} NOT found", name, namespace);
         is_set
     }
 }
