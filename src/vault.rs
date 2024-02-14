fd68475ab2c07261f75e29803df76d2d3e92c99a
bartvanbenthem <bartvanbenthem@hotmail.com>
2024-02-14 20:10:39 +0100

    update print vault secret

--- src/vault.rs
+++ src/vault.rs
@@ -26,47 +26,6 @@ impl AzureVault {
 
     // test the connection en authentication to the azure keyvault
     pub async fn test_connection() {}
-
-    pub async fn print_secret_from_vault(az: &AzureVault, secret_name: &str) {
-        let config = AzureVault {
-            tenant: az.tenant.clone(),
-            url: az.url.clone(),
-            spn: az.spn.clone(),
-        };
-
-        let spn_secret: String = env::var("SPN_SECRET").unwrap_or("none".to_string());
-
-        let creds = Arc::new(ClientSecretCredential::new(
-            new_http_client(),
-            config.tenant,
-            config.spn,
-            spn_secret,
-            TokenCredentialOptions::default(),
-        ));
-
-        let client_result = SecretClient::new(&config.url, creds);
-        let client = match client_result {
-            Ok(client) => client,
-            Err(error) => {
-                eprintln!("Error creating new Azure Secret CLient {}", error);
-                process::exit(1)
-            }
-        };
-
-        if secret_name.len() > 0 {
-            let secret_result = client.clone().get(secret_name).await;
-
-            let value = match secret_result {
-                Ok(s) => s.value,
-                Err(error) => {
-                    eprintln!("Error getting Azure Secrets from Client {}", error);
-                    process::exit(1);
-                }
-            };
-
-            println!("\nvalue from keyvault: {}\n", value);
-        }
-    }
 }
 
 pub async fn run(client: Client, name: &str, namespace: &str) {
@@ -107,5 +66,46 @@ async fn secret_value_is_set(client: Client, name: &str, namespace: &str, key: &
     } else {
         error!("Secret {} in namespace {} NOT found", name, namespace);
         is_set
+    }
+}
+
+pub async fn print_secret_from_vault(az: &AzureVault, secret_name: &str) {
+    let config = AzureVault {
+        tenant: az.tenant.clone(),
+        url: az.url.clone(),
+        spn: az.spn.clone(),
+    };
+
+    let spn_secret: String = env::var("SPN_SECRET").unwrap_or("none".to_string());
+
+    let creds = Arc::new(ClientSecretCredential::new(
+        new_http_client(),
+        config.tenant,
+        config.spn,
+        spn_secret,
+        TokenCredentialOptions::default(),
+    ));
+
+    let client_result = SecretClient::new(&config.url, creds);
+    let client = match client_result {
+        Ok(client) => client,
+        Err(error) => {
+            eprintln!("Error creating new Azure Secret CLient {}", error);
+            process::exit(1)
+        }
+    };
+
+    if secret_name.len() > 0 {
+        let secret_result = client.clone().get(secret_name).await;
+
+        let value = match secret_result {
+            Ok(s) => s.value,
+            Err(error) => {
+                eprintln!("Error getting Azure Secrets from Client {}", error);
+                process::exit(1);
+            }
+        };
+
+        println!("\nvalue from keyvault: {}\n", value);
     }
 }
--- tests/vault.rs
+++ tests/vault.rs
@@ -9,5 +9,5 @@ async fn print_secret_works() {
     let secret_name = env::var("SECRET_NAME").unwrap_or("none".to_string());
 
     let azure = AzureVault::new(&tenant, &keyvault_url, &spn);
-    AzureVault::print_secret_from_vault(&azure, &secret_name).await;
+    print_secret_from_vault(&azure, &secret_name).await;
 }
