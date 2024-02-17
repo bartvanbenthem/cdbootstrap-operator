e247cd682fba0d5152e2162df43c3bd731b91aad
bartvanbenthem <bartvanbenthem@hotmail.com>
2024-02-17 10:17:41 +0100

    structure test

--- tests/vault.rs
+++ tests/vault.rs
@@ -1,5 +1,50 @@
+use azure_core::new_http_client;
+use azure_identity::{ClientSecretCredential, TokenCredentialOptions};
+use azure_security_keyvault::SecretClient;
 use cdbootstrap::vault::*;
-use std::env;
+use std::sync::Arc;
+use std::{env, process};
+
+pub async fn print_secret_from_vault(az: &AzureVault, secret_name: &str) {
+    let config = AzureVault {
+        tenant: az.tenant.clone(),
+        url: az.url.clone(),
+        spn: az.spn.clone(),
+    };
+
+    let spn_secret: String = env::var("SPN_SECRET").unwrap_or("none".to_string());
+
+    let creds = Arc::new(ClientSecretCredential::new(
+        new_http_client(),
+        config.tenant,
+        config.spn,
+        spn_secret,
+        TokenCredentialOptions::default(),
+    ));
+
+    let client_result = SecretClient::new(&config.url, creds);
+    let client = match client_result {
+        Ok(client) => client,
+        Err(error) => {
+            eprintln!("Error creating new Azure Secret CLient {}", error);
+            process::exit(1)
+        }
+    };
+
+    if secret_name.len() > 0 {
+        let secret_result = client.clone().get(secret_name).await;
+
+        let value = match secret_result {
+            Ok(s) => s.value,
+            Err(error) => {
+                eprintln!("Error getting Azure Secrets from Client {}", error);
+                process::exit(1);
+            }
+        };
+
+        println!("\nvalue from keyvault: {}\n", value);
+    }
+}
 
 #[tokio::test]
 async fn print_secret_works() {
